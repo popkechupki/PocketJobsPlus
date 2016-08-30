0146f6bc3bab44fd8887b995a10866a05656667a
popkechupki <hobbypopkechupki@gmail.com>
2016-08-30 20:56:14 +0900

    break.ymlの生成部分

--- PocketJobsPlus/src/popkechupki/PocketJobsPlus.php
+++ PocketJobsPlus/src/popkechupki/PocketJobsPlus.php
@@ -12,118 +12,73 @@ use pocketmine\utils\Config;
 class PocketJobsPlus extends PluginBase implements Listener {
 
 	public function onEnable() {   
-    $this->getServer()->getPluginManager()->registerEvents($this,$this);
-    $this->getLogger()->info(TextFormat::GREEN."PocketJobsPlusを読み込みました".TextFormat::GOLD." By popkechupki");
-    $this->getLogger()->info(TextFormat::RED."このプラグインはpopke LICENSEに同意した上で使用してください。");
-    
+        $this->getServer()->getPluginManager()->registerEvents($this,$this);
+    /*Default Messages*/
+        $this->getLogger()->info(TextFormat::GREEN."PocketJobsPlusを読み込みました".TextFormat::GOLD." By popkechupki");
+        $this->getLogger()->info(TextFormat::RED."このプラグインはpopke LICENSEに同意した上で使用してください。");
+    /*Create Config Files*/
+        if (!file_exists($this->getDataFolder())) @mkdir($this->getDataFolder(), 0740, true);
+        $this->break = new Config($this->getDataFolder()."break.yml", Config::YAML, 
+            array(
+                '焼き石' => array(
+                    array('ID' => 1, 'meta' => 0, 'amount' => 2 ),
+                '草ブロック' => array(
+                    array('ID' => 2, 'meta' => 0, 'amount' => 2 ),
+                '土' => array(
+                    array('ID' => 3, 'meta' => 0, 'amount' => 2 ),
+                '丸石' => array(
+                    array('ID' => 4, 'meta' => 0, 'amount' => 10 ),
+                '木材' => array(
+                    array('ID' => 5, 'meta' => 0, 'amount' => 10 ),
+                '砂' => array(
+                    array('ID' => 12, 'meta' => 0, 'amount' => 2 ),
+                '砂利' => array(
+                    array('ID' => 13, 'meta' => 0, 'amount' => 2 ),
+                '金鉱石' => array(
+                    array('ID' => 14, 'meta' => 0, 'amount' => 30 ),
+                '鉄鉱石' => array(
+                    array('ID' => 15, 'meta' => 0, 'amount' => 30 ),
+                '石炭鉱石' => array(
+                    array('ID' => 16, 'meta' => 0, 'amount' => 20 ),
+                '原木' => array(
+                    array('ID' => 17, 'meta' => 0, 'amount' => 20 ),
+                '葉ブロック' => array(
+                    array('ID' => 18, 'meta' => 0, 'amount' => 2 ),
+                'ラピスラズリ鉱石' => array(
+                    array('ID' => 21, 'meta' => 0, 'amount' => 25 ),
+                '砂岩' => array(
+                    array('ID' => 24, 'meta' => 0, 'amount' => 10 ),
+                'ダイヤモンド鉱石' => array(
+                    array('ID' => 56, 'meta' => 0, 'amount' => 100 ),
+                '小麦' => array(
+                    array('ID' => 59, 'meta' => 0, 'amount' => 5 ),
+                'レッドストーン鉱石' => array(
+                    array('ID' => 73, 'meta' => 0, 'amount' => 25 ),
+                '雪ブロック' => array(
+                    array('ID' => 80, 'meta' => 0, 'amount' => 10 ),
+                '粘土ブロック' => array(
+                    array('ID' => 82, 'meta' => 0, 'amount' => 5 ),
+                'かぼちゃ' => array(
+                    array('ID' => 86, 'meta' => 0, 'amount' => 5 ),
+                'ネザーラック' => array(
+                    array('ID' => 87, 'meta' => 0, 'amount' => 2 ),
+                'ソウルサンド' => array(
+                    array('ID' => 88, 'meta' => 0, 'amount' => 2 ),
+                'グロウストーン' => array(
+                    array('ID' => 89, 'meta' => 0, 'amount' => 25 ),
+                'スイカブロック' => array(
+                    array('ID' => 103, 'meta' => 0, 'amount' => 2 ),
+            ));
+        $this->paste = new Config($this->getDataFolder()."paste.yml", Config::YAML);
     /*PocketMoneyAPI Road*/
-    if($this->getServer()->getPluginManager()->getPlugin("PocketMoney") != null){
+        if($this->getServer()->getPluginManager()->getPlugin("PocketMoney") != null) {
             $this->PocketMoney = $this->getServer()->getPluginManager()->getPlugin("PocketMoney");
-        }else{
+        } else {
             $this->getServer()->getPluginManager()->disablePlugin($this);
         }
 	}
 
 	public function onPlayerlockBreak(BlockBreakEvent $event) {
 		$user = $event->getPlayer()->getName();
-			switch ($event->getBlock()->getId()) {
-				case"1":
-                    $price = $this->break->get("焼き石(1)");
-                    $this->PocketMoney->grantMoney($user, +$price);
-                break;
-                case"2":
-                    $price = $this->break->get("草ブロック(2)");
-                    $this->PocketMoney->grantMoney($user, +$price);
-                break;
-                case"3":
-                    $price = $this->break->get("土(3)");
-                    $this->PocketMoney->grantMoney($user, +$price);
-                break;
-                case"4":
-                    $price = $this->break->get("丸石(4)");
-                    $this->PocketMoney->grantMoney($user, +$price);
-                break;
-                case"5":
-                    $price = $this->break->get("木材(5)");
-                    $this->PocketMoney->grantMoney($user, +$price);
-                break;
-                case"12":
-                    $price = $this->break->get("砂(12)");
-                    $this->PocketMoney->grantMoney($user, +$price);
-                break;
-                case"13":
-                    $price = $this->break->get("砂利(13)");
-                    $this->PocketMoney->grantMoney($user, +$price);
-                break;
-                case"14":
-                    $price = $this->break->get("金鉱石(14)");
-                    $this->PocketMoney->grantMoney($user, +$price);
-                break;
-                case"15":
-                    $price = $this->break->get("鉄鉱石(15)");
-                    $this->PocketMoney->grantMoney($user, +$price);
-                break;
-                case"16":
-                    $price = $this->break->get("石炭鉱石(16)");
-                    $this->PocketMoney->grantMoney($user, +$price);
-                break;
-                case"17":
-                    $price = $this->break->get("原木(17)");
-                    $this->PocketMoney->grantMoney($user, +$price);
-                break;
-                case"18":
-                    $price = $this->break->get("葉ブロック(18)");
-                    $this->PocketMoney->grantMoney($user, +$price);
-                break;
-                case"21":
-                    $price = $this->break->get("ラピスラズリ鉱石(21)");
-                    $this->PocketMoney->grantMoney($user, +$price);
-                break;
-                case"24":
-                    $price = $this->break->get("砂岩(24)");
-                    $this->PocketMoney->grantMoney($user, +$price);
-                break;
-                case"56":
-                    $price = $this->break->get("ダイアモンド鉱石(56)");
-                    $this->PocketMoney->grantMoney($user, +$price);
-                break;
-                case"59":
-                    $price = $this->break->get("小麦(59)");
-                    $this->PocketMoney->grantMoney($user, +$price);
-                break;
-                case"73":
-                case"74":
-                    $price = $this->break->get("レッドストーン鉱石(73)");
-                    $this->PocketMoney->grantMoney($user, +$price);
-                break;
-                case"80":
-                    $price = $this->break->get("雪ブロック(80)");
-                    $this->PocketMoney->grantMoney($user, +$price);
-                break;
-                case"82":
-                    $price = $this->break->get("粘土(82)");
-                    $this->PocketMoney->grantMoney($user, +$price);
-                break;
-                case"86":
-                    $price = $this->break->get("かぼちゃ(86)");
-                    $this->PocketMoney->grantMoney($user, +$price);
-                break;
-                case"87":
-                    $price = $this->break->get("ネザーラック(87)");
-                    $this->PocketMoney->grantMoney($user, +$price);
-                break;
-                case"88":
-                    $price = $this->break->get("ソウルサンド(88)");
-                    $this->PocketMoney->grantMoney($user, +$price);
-                break;
-                case"89":
-                    $price = $this->break->get("グロウストーン(89)");
-                    $this->PocketMoney->grantMoney($user, +$price);
-                break;
-                case"103":
-                    $price = $this->break->get("スイカ(103)");
-                    $this->PocketMoney->grantMoney($user, +$price);
-                break;
-			}
 	}
 }
