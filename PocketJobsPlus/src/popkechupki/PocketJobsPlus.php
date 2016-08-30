cf594facdefd29aaaee0447c53fc57d478c19c2f
popkechupki <hobbypopkechupki@gmail.com>
2016-08-30 19:53:06 +0900

    Update PocketJobsPlus.php

--- PocketJobsPlus/src/popkechupki/PocketJobsPlus.php
+++ PocketJobsPlus/src/popkechupki/PocketJobsPlus.php
@@ -2,43 +2,26 @@
 
 namespace popkechupki;
 
-//server
-use pocketmine\Server;
-
-//player
 use pocketmine\Player;
-
-//plugin
 use pocketmine\plugin\PluginBase;
-
-//event
 use pocketmine\event\Listener;
 use pocketmine\event\block\BlockBreakEvent;
-
-//another
 use pocketmine\utils\TextFormat;
 use pocketmine\utils\Config;
 
 class PocketJobsPlus extends PluginBase implements Listener {
 
-	public function onEnable() {                                    
+	public function onEnable() {   
+    $this->getServer()->getPluginManager()->registerEvents($this,$this);
     $this->getLogger()->info(TextFormat::GREEN."PocketJobsPlusを読み込みました".TextFormat::GOLD." By popkechupki");
     $this->getLogger()->info(TextFormat::RED."このプラグインはpopke LICENSEに同意した上で使用してください。");
-    if (!file_exists($this->getDataFolder())) @mkdir($this->getDataFolder(), 0741, true);
-        $this->break = new Config($this->getDataFolder() . "break.yml", Config::YAML,
-                    array(
-                        '焼き石(1)'=>'2', '草ブロック(2)'=>'2', '土(3)'=>'2', '丸石(4)'=>'10', '木材(5)'=>'10', '砂(12)'=>'2', '砂利(13)'=>'2', '金鉱石(14)'=>'30', '鉄鉱石(15)'=>'30', '石炭鉱石(16)'=>'20', '原木(17)'=>'20', '葉ブロック(18)'=>'2', 'ラピスラズリ鉱石(21)'=>'25', '砂岩(24)'=>'10', 'ダイアモンド鉱石(56)'=>'100', '小麦(59)'=>'5', 'レッドストーン鉱石(73)'=>'25', '雪ブロック(80)'=>'10', '粘土(82)'=>'2', 'かぼちゃ(86)'=>'5', 'ネザーラック(87)'=>'2', 'ソウルサンド(88)'=>'2', 'グロウストーン(89)'=>'25', 'スイカ(103)'=>'2'
-                        ));
+    
     /*PocketMoneyAPI Road*/
     if($this->getServer()->getPluginManager()->getPlugin("PocketMoney") != null){
             $this->PocketMoney = $this->getServer()->getPluginManager()->getPlugin("PocketMoney");
-            $this->getLogger()->info("PocketMoneyを検出しました。");
         }else{
-            $this->getLogger()->warning("PocketMoneyが見つかりませんでした");
             $this->getServer()->getPluginManager()->disablePlugin($this);
         }
-    $this->break->save();
-    $this->getServer()->getPluginManager()->registerEvents($this,$this);
 	}
 
 	public function onPlayerlockBreak(BlockBreakEvent $event) {
